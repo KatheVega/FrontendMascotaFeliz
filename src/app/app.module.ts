c2746278ff118836d1106acb395ef56f767268be
KatheVega <kathevega03@gmail.com>
2022-11-21 01:14:35 -0500

    Sprint 3 implementacion de frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PiePaginaComponent } from './plantillas/pie-pagina/pie-pagina.component';
+import { ErrorComponent } from './plantillas/error/error.component';
+import { Error500Component } from './plantillas/error500/error500.component';
+import { Error404Component } from './plantillas/error404/error404.component';
+import { HomeComponent } from './plantillas/home/home.component';
+import { BarraNavegacionComponent } from './plantillas/barra-navegacion/barra-navegacion.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraNavegacionComponent,
+    PiePaginaComponent,
+    ErrorComponent,
+    Error500Component,
+    Error404Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
